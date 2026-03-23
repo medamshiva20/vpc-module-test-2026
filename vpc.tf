210be021ed37af1899e11efc1b9a611591ce162a
medamshiva20 <medamshiva20@gmail.com>
2026-03-23 19:05:14 +0530

    vpc peering connection

--- vpc.tf
+++ vpc.tf
@@ -2,4 +2,5 @@ module "vpc"{
     source = "git::https://github.com/medamshiva20/terraform-aws-vpc-2026.git?ref=main"
     project = var.project
     environment = var.environment
+    is_peering_required = true
 }
